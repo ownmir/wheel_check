9e8be8c20b342c37946f529ea31006a996ffa134
ownmir <ownmir@i.ua>
2020-03-02 12:32:13 +0200

    pack 002

--- pack.py
+++ pack.py
@@ -7,14 +7,14 @@ data = {}
 with open('itysk' + '.pkl', 'wb') as to:
 	line_list = []
 	for tup in tree:
-                for address, dirs, files in tup:
-                        if '.' in adress:
-                                continue
-                        for file in files:
-                                path = adress + '/' + file
-                                print(path)
-                                with open(path, 'r') as cur:
-                                        for line in cur:
-                                                line_list.append(line)
-                                data[path] = line_list
-        pickle.dump(data, to)
+		for address, dirs, files in tup:
+				if '.' in adress:
+						continue
+				for file in files:
+						path = adress + '/' + file
+						print(path)
+						with open(path, 'r') as cur:
+								for line in cur:
+										line_list.append(line)
+						data[path] = line_list
+	pickle.dump(data, to)
