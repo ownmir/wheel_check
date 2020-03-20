4ad4d8f0ed55953e473296d0826ebcd8af1182b2
ownmir <ownmir@i.ua>
2020-03-20 19:44:14 +0200

    pack003

--- pack.py
+++ pack.py
@@ -1,20 +1,40 @@
+import os
 import pickle
-from os import walk
 
-tree = walk('itysk')
 
-data = {}
-with open('itysk' + '.pkl', 'wb') as to:
-	line_list = []
-	for tup in tree:
-		for address, dirs, files in tup:
-				if '.' in adress:
-						continue
-				for file in files:
-						path = adress + '/' + file
-						print(path)
-						with open(path, 'r') as cur:
-								for line in cur:
-										line_list.append(line)
-						data[path] = line_list
-	pickle.dump(data, to)
+def to():
+    tree = os.walk('itest')
+    tree_list = []
+    for i in tree:
+        print(i)
+        tree_list.append(i)
+    for address, folders, files in tree_list:
+        if '__pycache__' in address:
+            continue
+        for file in files:
+            print(address + '\\' + file)
+            old = 'd:\\pyprg\\' + address + '\\' + file
+            new = 'd:\\pyprg\\to\\' + address + '\\' + file
+            rf = open(old, 'rb')
+            print(rf)
+            os.system('cd to')
+            if os.path.exists('d:\\pyprg\\to\\' + address):
+                print("Указанный файл существует") 
+            else:
+                os.mkdir('d:\\pyprg\\to\\' + address)
+            wf = open(new, 'wb')
+            print(wf)
+            while True:
+                data = rf.read(1024)
+                print(data)
+                if not data:
+                    rf.close()
+                    wf.close()
+                    os.system('cd ..')
+                    break
+                
+                wf.write(data)
+
+
+if __name__ == '__main__':
+    to()
