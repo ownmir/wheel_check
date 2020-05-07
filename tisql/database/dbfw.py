fc1e680250498992fdf7f1f91a16ba2f4c7e6169
fominvv <jove@ukr.net>
2020-05-07 21:06:03 +0300

    sqlparse0705

--- tisql/database/dbfw.py
+++ tisql/database/dbfw.py
@@ -1,7 +1,7 @@
 
 
-def connect_to_base_and_execute(query, error_label, gui="pyqt", base="oracle"):
-    print("query", query)
+def connect_to_base_and_execute(query, error_label, user, password, parse_button, gui="pyqt", base="oracle"):
+
     if gui == "tkinter":
         error_label['text'] = ''
     else:
@@ -18,7 +18,50 @@ def connect_to_base_and_execute(query, error_label, gui="pyqt", base="oracle"):
         print('Welcome to Oracle driver!')
     else:
         print('Welcome to else driver!')
-
+    try:
+        connection = ora.connect(user, password, 'MMFO')
+        print("Connect is created.")
+        if gui == "tkinter":
+            parse_button['text'] = 'Список МФО, запрос (Пользователь и пароль должны быть правильными.)'
+        else:
+            parse_button.setText("Запрос (Пользователь и пароль должны быть правильными.)")
+        result = ''
+        with connection.cursor() as cursor:
+            print("Cursor is created.")
+            # так объявляется курсорная переменная
+            # ret = cursor.var(ora.CURSOR)
+            # cursor.execute('''begin test_cur(1, 20, :ret); end; ''', ret=ret)
+            cursor.execute('''begin  bars.bars_login.login_user(sys_guid, 1, null, null); end; ''')
+            print("Bars login.")
+            # print(cursor)
+            try:
+                print("After query", query)
+                for row in cursor.execute(query):
+                    result += '{}\n'.format(row)
+                print("Executed.")
+            except ora.DatabaseError as query_error:
+                if gui == "tkinter":
+                    parse_button['text'] = 'Список МФО, запрос (БЫЛА ОШИБКА ЗАПРОСА!)'
+                else:
+                    parse_button.setText("Запрос (БЫЛА ОШИБКА ЗАПРОСА!)")
+                print('Query error: ', query_error)
+        if result == '':
+            if gui == "tkinter":
+                error_label['text'] = 'Нет данных по блокированным.'
+            else:
+                error_label.setText('Нет данных')
+        else:
+            if gui == "tkinter":
+                error_label['text'] = result
+            else:
+                error_label.setText(result)
+    except ora.DatabaseError as connect_error:
+        if gui == "tkinter":
+            parse_button['text'] = 'Список МФО, запрос (БЫЛА ОШИБКА СОЕДИНЕНИЯ!)'
+        else:
+            parse_button.setText("Запрос (БЫЛА ОШИБКА СОЕДИНЕНИЯ!)")
+        print('Connect error: ', connect_error)
+    print("query", query)
 
 
 if __name__ == '__main__':
--- tisql/framework_base.py
+++ tisql/framework_base.py
@@ -1,15 +1,44 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QGridLayout, QAction, QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QTabWidget, QPushButton, QSplitter, QTableWidget, QTableWidgetItem, QTextEdit, QLabel
+from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from database.dbfw import connect_to_base_and_execute
+import sqlparse
 
     
-def run(error_label):
-    print('In run')
-    print(error_label.text())
-    connect_to_base_and_execute("select * from dummy;", error_label)
+def run(index, text1, text2, error_label, user, password, parse_button, table):
+    # print(error_label.text())
+    if index == 0:
+        query = text1.toPlainText()
+    else:
+        query = text2.toPlainText()
+    try:
+        parsed = sqlparse.parse(query)[0]
+        if parsed.get_type() == 'SELECT':
+            if password == "d":
+                error_label.setText('Вы не ввели пароль!')
+            else:
+                error_label.setText('Пока все хорошо..')
+                connect_to_base_and_execute(query, error_label, user, password, parse_button)
+                table.clear()
+                try:
+                    id_list = list(parsed.tokens[2].get_identifiers())
+                    table.setColumnCount(len(id_list))  # Устанавливаем количество колонок, равное к-ву полей в запросе
+                    # table.setRowCount(1)  # и одну строку в таблице
+                    # Устанавливаем заголовки таблицы
+                    headers_list = [x.get_alias() for x in id_list]
+                    table.setHorizontalHeaderLabels(headers_list)
+                except AttributeError:
+                    error_label.setText('Что-то не так(')
+
+                    tokens = parsed.tokens
+
+        else:
+            error_label.setText('В качестве запросов вы можете использовать только выборки "SELECT"!')
+    except IndexError:
+        error_label.setText('Пустой запрос?')
 
 
 class Main(QMainWindow):
@@ -21,6 +50,11 @@ class Main(QMainWindow):
 
     def top_on_current_change(self, index):
         print("Top index", index)
+        if index == 0:
+            # print(type(self.ok_button))
+            pass
+        elif index == 1:
+            pass
 
     def bottom_on_current_change(self, index):
         print("Bottom index", index)
@@ -30,28 +64,38 @@ class Main(QMainWindow):
         
         central_widget = QWidget(self)                  # Создаём центральный виджет
         self.setCentralWidget(central_widget)
-        ok_button = QPushButton("OK")
+        self.ok_button = QPushButton("OK")
 
-        cancel_button = QPushButton("Cancel")
+        self.cancel_button = QPushButton("Результат смотри тут")
 
-        error_label = QLabel("Errors will be here")
-        cancel_button.clicked.connect(lambda: run(error_label))
+        self.error_label = QLabel("Errors will be here")
+
+        self.user_entry = QLineEdit("user5301")
+        self.pass_entry = QLineEdit("d")
+        self.pass_entry.setEchoMode(QLineEdit.PasswordEchoOnEdit)
         vbox = QVBoxLayout(central_widget)
 
         top = QTabWidget(central_widget)
-        text_edit1 = QTextEdit()
-        tab1 = top.addTab(text_edit1, "Tab1")
-        print('tab1', tab1, 'type(tab1)', type(tab1))
-        text_edit2 = QTextEdit()
-        tab2 = top.addTab(text_edit2, "Tab2")
-        print('tab2', tab2, 'type(tab1)', type(tab2))
+        q = """select a.fn "Файл", a.dat "dat", a.kv "Валюта", a.skr "Сума файла", a.n "К-во пл", a.otm "otm", a.k_er "Ошибка", b.nb "Банк отправителя", a.sign_key "Ключ" from bars.zag_b a, bars.banks$base b where  a.DAT >= trunc(sysdate) and a.kf = b.mfo and a.otm <= 3 and to_char(a.dat + INTERVAL '20' MINUTE,'DD.MM.YYYY HH24:MI') <= to_char(sysdate,'DD.MM.YYYY HH24:MI') group by  fn, a.dat, a.kv, a.skr, a.n, a.otm, a.k_er, b.nb, a.kf, a.sign_key order by a.kf, a.dat"""
+        print("q", q, "end q")
+        self.text_edit1 = QTextEdit()
+        self.text_edit1.setPlainText(q)
+        # ok_button.clicked.connect(lambda: run(q, error_label, user_entry.text(), pass_entry.text(), cancel_button))
+        # self.ok_button.clicked.connect(lambda: run(self.text_edit1.toPlainText(), self.error_label, self.user_entry.text(), self.pass_entry.text(), self.cancel_button))
+        top.addTab(self.text_edit1, "Файлы, не сквитованые более 20 мин")
+        self.text_edit2 = QTextEdit()
+        top.addTab(self.text_edit2, "Tab2")
         top.currentChanged.connect(self.top_on_current_change)
+
         bottom = QTabWidget(central_widget)
         bottom.setTabPosition(QTabWidget.South)  # внизу
 
         # grid_layout = QGridLayout()             # Создаём QGridLayout
         # bottom.setLayout(grid_layout)
         table = QTableWidget(self)  # Создаём таблицу
+        self.ok_button.clicked.connect(
+            lambda: run(top.currentIndex(), self.text_edit1, self.text_edit2, self.error_label, self.user_entry.text(),
+                        self.pass_entry.text(), self.cancel_button, table))
         table.setColumnCount(3)  # Устанавливаем три колонки
         table.setRowCount(1)  # и одну строку в таблице
 
@@ -87,9 +131,11 @@ class Main(QMainWindow):
 
         vbox.addWidget(splitter_vertical)
         hbox = QHBoxLayout(central_widget)
-        hbox.addWidget(ok_button)
-        hbox.addWidget(cancel_button)
-        hbox.addWidget(error_label)
+        hbox.addWidget(self.ok_button)
+        hbox.addWidget(self.cancel_button)
+        hbox.addWidget(self.user_entry)
+        hbox.addWidget(self.pass_entry)
+        hbox.addWidget(self.error_label)
         hbox.addStretch(1)
         vbox.addLayout(hbox)
         central_widget.setLayout(vbox)
