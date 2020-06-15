1dcb610de29f3bb375452b24e4fe8915fc21a326
Volodymyr Fomin <ownmir@i.ua>
2020-06-15 18:16:45 +0300

    pysideodbc1506

--- ProhDron/runo.py
+++ ProhDron/runo.py
@@ -1,14 +1,14 @@
 from PySide2 import QtWidgets
 import sys
-from wwdb import WorkWithLiteDB, WorkWithMyDB
+from wwdb import WorkWithLiteDB, WorkWithMyDB, WorkWithODBC
 
 app = QtWidgets.QApplication(sys.argv)
 # e = WorkWithLiteDB("D:/Work/Py3proj/duties/a.db")
 # e = WorkWithLiteDB("D:/Work/Py3proj/a.db")
-e = WorkWithMyDB("adb", "3306", "neo", "pp12")
+e = WorkWithODBC("MySQL ODBC 5.3 Unicode Driver", "MY", "localhost", 3306, "root", "322067")
+# e = WorkWithODBC("MySQL ODBC 5.3 Unicode Driver", "XESN", "localhost", 1521, "user5301", "us322067")
 print("Name of database:", e.name)
 connn = e.open_db()
 if connn:
-    print(e.name, "database is opened")
+    print(e.name, "Database is opened.")
     connn.close()
-    
--- ProhDron/wwdb.py
+++ ProhDron/wwdb.py
@@ -1,6 +1,7 @@
 from PySide2.QtSql import QSqlDatabase
 from abc import ABC, abstractmethod
 
+
 class WorkWithDB(ABC):
     def __init__(self, name):
         self.__name = name
@@ -18,6 +19,7 @@ class WorkWithDB(ABC):
         self.conn = QSqlDatabase.addDatabase(kind)
         self.conn.setDatabaseName(self.name)
 
+
 class WorkWithLiteDB(WorkWithDB):
     
     def open_db(self):
@@ -26,15 +28,16 @@ class WorkWithLiteDB(WorkWithDB):
         if self.conn.open():
             return self.conn
         else:
-            print("Была ошибка открытия базы SQLITE с именем", self.__name)
-            self.conn.lastError().text()
+            print("Была ошибка открытия базы SQLITE с именем", self.__name, self.conn.lastError().text())
             return
 
+
 class WorkWithMyDB(WorkWithDB):
-    def __init__(self, name, host, user, passw):
+    def __init__(self, name, host, port, user, passw):
         WorkWithDB.__init__(self, name)
         self.__name = name
         self.__host = host
+        self.__port = port
         self.__user = user
         self.__pass = passw
 
@@ -43,18 +46,61 @@ class WorkWithMyDB(WorkWithDB):
         return self.__host
 
     @property
+    def port(self):
+        return self.__port
+
+    @property
     def user(self):
         return self.__user
 
-    def open_db(self):
-        self.add('QMYSQL')
+    def set_general_options(self):
         self.conn.setHostName(self.host)
+        self.conn.setPort(self.port)
         # conn.setDatabaseName(self.name)
         self.conn.setUserName(self.user)
         self.conn.setPassword(self.__pass)
+
+    def _output(self):
+        print("Подробности: isOpen()", self.conn.isOpen(), "| isOpenError()", self.conn.isOpenError(),
+              "| lastError()", self.conn.lastError(), "| lastError().text()", self.conn.lastError().text(),
+              "| lastError().databaseText() - DB", self.conn.lastError().databaseText(),
+              "| lastError().driverText() - Qt", self.conn.lastError().driverText(),
+              "| connectionName()", self.conn.connectionName(), "Доступные драйверы", self.conn.drivers()
+              )
+
+    def open_db(self):
+        self.add('QMYSQL')
+        self.set_general_options()  # host, port, user, passw
+
         if self.conn.open():
+            self._output()
             return self.conn
         else:
             print("Была ошибка открытия базы mysql с именем", self.name)
-            self.conn.lastError().text()
+            self._output()
+            print("Db and Qt errors with space / Ошибки Db и Qt через пробел", self.conn.lastError().text())
+            return
+
+
+class WorkWithODBC(WorkWithMyDB):
+    def __init__(self, driver, name, host, port, user, passw):
+        WorkWithMyDB.__init__(self, name, host, port, user, passw)
+        self.__driver = driver
+        self.__pass = passw
+
+    @property
+    def driver(self):
+        return self.__driver
+
+    def open_db(self):
+        self.conn = QSqlDatabase.addDatabase("QODBC3")
+        self.conn.setDatabaseName("{{Driver={0}}};DSN={1};UID={2};PWD={3}".format(self.driver, self.name, self.user,
+                                                                                  self.__pass))
+        if self.conn.open():
+            self._output()
+            return self.conn
+        else:
+            print("Была ошибка открытия базы через odbc с именем", self.name)
+            self._output()
+            print("Db and Qt errors with space / Ошибки Db и Qt через пробел", self.conn.lastError().text())
             return
