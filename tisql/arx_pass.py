49ac293efb578d9f48188df3f85a3a0b23dbfbbc
fominvv <jove@ukr.net>
2020-08-03 19:28:45 +0300

    arxsec

--- tisql/arx_pass.py
+++ tisql/arx_pass.py
@@ -1,8 +1,12 @@
+import sys
 import subprocess
 import hashlib
 
+sys.path.append(r"C:\Program Files\7-zip")
+sys.path.append(r"D:\TMP")
 #save
 filename = 'mani.py'
+filesalt = ''
 with open(filename, 'rb') as f:
     m = hashlib.md5()
     while True:
@@ -17,11 +21,11 @@ print(myhash)
 za = subprocess.call(['7z', 'a', '-P'+myhash, 'avv', filename])
 print(za)
 # test
-with open("h", "r", encoding='utf-8') as tst:
+with open("hdatabase", "r", encoding='utf-8') as tst:
    lines = f.readlines()
    try:
        self.interval_e.setText(lines[0])
    except:
-       self.error_label.setText(' Возможно в первой строке файла h нет line!')
+       self.error_label.setText(' Возможно в первой строке файла hdatabase нет line!')
 input("Modification of " + filename + " file is blocked")
 
